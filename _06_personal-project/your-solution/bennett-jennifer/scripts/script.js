443f993c1e90db19b4953ac01d6425c36785fabc
Jennifer Bennett <jennbbennett@hotmail.com>
2015-10-17 16:47:28 -0600

    Completed personal webpages

---
+++ _06_personal-project/your-solution/bennett-jennifer/scripts/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	$("#button1").click(function() {
+		$("#frustration").toggle();
+	});
+	$("#button2").click(function() {
+		$("#dancing").toggle();
+	});
+});
+
+
+
